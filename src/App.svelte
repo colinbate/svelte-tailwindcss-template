e36996fe4616e8d43c396c19760b76e5f973954c
Colin Bate <colin@colinbate.com>
2020-06-05 14:17:08 -0300

    Switch boilerplate styles to use Tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -4,34 +4,12 @@
   export let name;
 </script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
 <Tailwindcss />
-<main>
-  <h1>Hello {name}!</h1>
+<main class="p-4 mx-auto text-center max-w-xl">
+  <h1 class="uppercase text-6xl font-hairline text-svelte">Hello {name}!</h1>
   <p>
     Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    <a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
     to learn how to build Svelte apps.
   </p>
 </main>
