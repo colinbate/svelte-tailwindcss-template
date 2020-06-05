c70ddd29fd5747f18240e9ba2b48bd6696ec0315
Colin Bate <colin@colinbate.com>
2020-06-05 14:24:24 -0300

    Add Svelte theme colour and neutral greys.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,19 +10,33 @@ module.exports = {
         light: { raw: "(prefers-color-scheme: light)" },
         dark: { raw: "(prefers-color-scheme: dark)" }
       },
+      colors: {
+        neutral: {
+          '100': '#f5f5f5',
+          '200': '#eeeeee',
+          '300': '#d2d2d2',
+          '400': '#aaaaaa',
+          '500': '#888888',
+          '600': '#707070',
+          '700': '#555555',
+          '800': '#333333',
+          '900': '#202020',
+        },
+        svelte: '#ff3e00',
+      }
     }
   },
   variants: {},
   plugins: [function({ addBase, config }) {
     addBase({
       body: {
-        color: config("theme.colors.gray.800"),
+        color: config("theme.colors.neutral.800"),
         backgroundColor: config("theme.colors.white")
       },
       "@screen dark": {
         body: {
           color: config("theme.colors.white"),
-          backgroundColor: config("theme.colors.gray.800")
+          backgroundColor: config("theme.colors.neutral.800")
         }
       }
     });
